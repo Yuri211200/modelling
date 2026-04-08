297e9584645dda049238f1c2fc2207e33bc2aeb5
Yuri Alves <yurialvesfr21@gmail.com>
2026-04-08 08:05:46 -0300

    2D Models Final Commit

--- 2D_Model.py
+++ 2D_Model.py
@@ -26,7 +26,7 @@ plt.plot(wavelet)
 plt.show()
 
 #Camadas
-v = [1500, 2000]
+v = [1000, 2500]
 
 camadas = np.zeros((nz, nx))
 camadas[:nz//2, :] = v[0]
@@ -50,10 +50,6 @@ u = np.zeros((nz, nx))
 u_prev = np.zeros((nz, nx))
 u_next = np.zeros((nz, nx))
 
-# fig, ax = plt.subplots()
-# img = ax.imshow(u, cmap='viridis', vmin=-1, vmax=1)
-# plt.colorbar(img)
-
 #Diferenças Finitas
 @njit(nopython=True, parallel=True)
 def DF(u, nx, nz, dx, dz):
@@ -68,72 +64,20 @@ def DF(u, nx, nz, dx, dz):
             pxx = (c0 * u[j, i] + c1 * (u[j, i+1] + u[j, i-1]) + c2 * (u[j, i+2] + u[j, i-2]) + c3 * (u[j, i+3] + u[j, i-3]) +c4 * (u[j, i+4] + u[j, i-4])) / (dx * dx)
             pzz = (c0 * u[j, i] + c1 * (u[j+1, i] + u[j-1, i]) + c2 * (u[j+2, i] + u[j-2, i]) + c3 * (u[j+3, i] + u[j-3, i]) + c4 * (u[j+4, i] + u[j-4, i])) / (dz * dz)
             laplacian[j, i] = pxx + pzz
-    # img.set_array(u)
-    
-    #laplacian = pxx + pzz
 
-    return laplacian #, [img]
+    return laplacian 
 
+# Propagação a partir da fonte    
 for t in range(nt):
-    # Propagação a partir da fonte    
     u[fz, fx] += wavelet[t]
     laplacian = DF(u, nx, nz, dx, dz)
     u_next = 2*u - u_prev + (camadas**2)*(dt**2)*laplacian
-    #u_prev = (camadas ** 2) * (dt ** 2) * laplacian + 2 * u - u_next
     snapshot[t, :, :] = u
 
-    if t % 300 == 0:
+    if t % 100 == 0:
         plt.imshow(u)
         plt.show()
 
     # Atualiza estados
     u_prev = u
     u = u_next
-
-
-# ani = FuncAnimation(fig, DF, frames=10000, interval=200)
-"""
-@njit(parallel=True, fastmath=True)
-def _forward_kernel(
-  upas: np.ndarray,
-  upre: np.ndarray,
-  ufut: np.ndarray,
-  laplacian: np.ndarray,
-  inv_dh2: float,
-  nzz: int,
-  nxx: int,
-  ricker: np.ndarray,
-  ix: int,
-  iz: int,
-  dh2: float,
-  arg: np.ndarray,
-  t: int,
-) -> None:
-
-  
-  upre[iz, ix] += ricker[t] / dh2
-
-  for i in prange(4, nzz - 4):
-    for j in range(4, nxx - 4):
-      d2u_dx2 = (
-        -9.0   * upre[i-4, j] + 128.0   * upre[i-3, j] - 1008.0 * upre[i-2, j] +
-        8064.0 * upre[i-1, j] - 14350.0 * upre[i,   j] + 8064.0 * upre[i+1, j] -
-        1008.0 * upre[i+2, j] + 128.0   * upre[i+3, j] - 9.0    * upre[i+4, j]
-      )
-
-      d2u_dz2 = (
-        -9.0   * upre[i, j-4] + 128.0   * upre[i, j-3] - 1008.0 * upre[i, j-2] +
-        8064.0 * upre[i, j-1] - 14350.0 * upre[i, j]   + 8064.0 * upre[i, j+1] -
-        1008.0 * upre[i, j+2] + 128.0   * upre[i, j+3] - 9.0    * upre[i, j+4]
-      )
-
-      laplacian[i, j] = (d2u_dx2 + d2u_dz2) * inv_dh2
-
-  for i in prange(4, nzz - 4):
-    for j in range(4, nxx - 4):
-
-      upas[i, j] = arg[i, j] * laplacian[i, j] + 2.0 * upre[i, j] - ufut[i, j]
-
-      ufut[i, j] = upre[i, j]
-      upre[i, j] = upas[i, j]
-"""
