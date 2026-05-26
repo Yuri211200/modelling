9e4288607ad87973d00dc16bbf79f3775ae61075
Yuri Alves <yurialvesfr21@gmail.com>
2026-05-26 10:44:23 -0300

    Minor fixes

--- 2D_Model.py
+++ 2D_Model.py
@@ -27,12 +27,12 @@ plt.plot(wavelet)
 plt.show()
 
 #Função para criar uma elipse
-def create_ellipse(center, a, b):
+def create_elipse(center, a, b):
     y, x = np.ogrid[-center[0]:nz-center[0], -center[1]:nx-center[1]]
     elipse = ((x**2)/a**2) + (y**2/b**2) <= 1
     return elipse
 
-ellipse = create_ellipse((155, 150), 125, 5)
+elipse = create_elipse((155, 150), 125, 5)
 
 #Camadas
 v = [1500, 2500, 500]
@@ -41,11 +41,12 @@ camadas = np.zeros((nz, nx))
 camadas[:nz//2, :] = v[0]
 camadas[nz//2:nz, :] = v[1]
 
-#Atribuindo valores anomalos na região da elipse
+#Atribuindo valores anomalos na região da elipse e plot do modelo
 for i in range(nz):
     for j in range(nx):
-        if ellipse[i, j]:
+        if elipse[i, j]:
             camadas[i, j] = v[2]
+
 plt.figure()
 plt.imshow(camadas)
 plt.scatter(rx, rz, label = "receptor")
